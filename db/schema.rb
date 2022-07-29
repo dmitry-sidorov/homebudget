b8783cd1a00df350611757c42bd307b144fec48c
Сидоров Дмитрий Евгеньевич <Dzmitry.Sidarau@alfa-bank.by>
2022-07-29 22:17:54 +0300

    Add price to payment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_19_204924) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_29_191552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_19_204924) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "processing_date", null: false
+    t.float "price", null: false
     t.index ["category_id"], name: "index_payments_on_category_id"
     t.index ["user_id"], name: "index_payments_on_user_id"
   end
